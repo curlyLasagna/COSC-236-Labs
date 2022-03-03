f15e431bd73590984ffbe270998946aa1d0920a3
curlyLasagna <luis.gcodes@gmail.com>
2022-03-02 23:03:25 -0500

    Program 4 finished. Problem with scanner now

--- Lab5/Lab5.java
+++ Lab5/Lab5.java
@@ -1,7 +1,7 @@
 /**
  * <h1>Lab 5: Arrays</h1>
  * This lab implements different loops to be used for arrays 
- * @author Luis & Abdalla
+ * @author Driver: Luis G. Coder: Abdalla A.
  * 
  */
 import java.util.*;
@@ -45,14 +45,17 @@ public class Lab5 {
             break;
           default:
             System.err.println("Invalid option. Try again");
-            continue;
+            break;
         }
       } else {
         System.err.println("Invalid option. Try again");
-        /** 
-         * Advances the scanner to the next line, essentially flushing the scanner so invalid inputs do not cause an infinite loop.
+
+        /**
+         * Advances the scanner to the next line, essentially flushing the scanner so
+         * invalid inputs do not cause an infinite loop.
          * See https://stackoverflow.com/questions/10604125/how-can-i-clear-the-scanner-buffer-in-java
          */
+
         in.nextLine();
       }
 
@@ -60,133 +63,230 @@ public class Lab5 {
     in.close();
   }
 
-  /** */
+  /**
+   * Input temperature
+   * print if below or above freezing
+   * count total number of temperatures above and below freezing
+   * print average of all temperatures
+   */
   static void problem1() {
-    final int freezing = 32;
-    int temperature;
+    final int FREEZE = 32;
+    int 
+      temperature = 0,
+      belowFreezingCount = 0,
+      aboveFreezingCount = 0,
+      tempSum = 0;
+    boolean run = true;
+    double tempAvg = 0;
 
-    if(in.hasNextInt())
-      temperature = in.nextInt();
-
-    else
-      System.err.println("Invalid input. Try again");
-
+    do {
+      System.out.println("Input temperatures. Input any character to stop");
+      if (in.hasNextInt()) {
+        temperature = in.nextInt();
+        tempSum += temperature;
+        if (temperature > FREEZE) 
+          aboveFreezingCount++;
+        else 
+          belowFreezingCount++;
+      } else 
+        run = false;
+    } while (run);
+    tempAvg = tempSum/(aboveFreezingCount + belowFreezingCount);
+    System.out.println("Average temperature: " + tempAvg);
+    in.nextLine();
   }
 
   /**
    * Inputs test scores repeatedly until the user stops
    * Print the total count of letter grades and average score
-   * */
+   */
   static void problem2() {
-    System.out.println("Enter test scores. ");
+    int 
+      testScore,
+      sumGrade = 0;
+    double testScoreAvg;
+    boolean run = true;
+    /**
+     * Grade        Index
+     * A      =      0
+     * B      =      1
+     * ..     .
+     * F      =      5
+     */
+    int [] letterGradeCount = new int[5];
+    char [] letterGrade = { 'A', 'B', 'C', 'D', 'F', };
 
-  }
+    Arrays.fill(letterGradeCount, 0);
+
+    do {
+      System.out.println("Input test scores. Input any character to stop");
+
+      if (in.hasNextInt()) {
+        testScore = in.nextInt();
+        sumGrade += testScore;
+
+        // F
+        if(testScore < 60) 
+          letterGradeCount[letterGradeCount.length - 1]++;
+        
+        // D
+        else if (testScore < 70) 
+          letterGradeCount[letterGradeCount.length - 2]++;
+        
+        // C
+        else if (testScore < 80) 
+          letterGradeCount[letterGradeCount.length - 3]++;
+        
+        // B
+        else if (testScore < 90) 
+          letterGradeCount[letterGradeCount.length - 4]++;
+        
+        // A
+        else
+          letterGradeCount[letterGradeCount.length - 5]++;
+        }  
+
+        else
+          run = false;
+      } while (run);
+
+        int totalGradeCount = 0;
+
+        // Sum the number of grades entered
+        for (int i : letterGradeCount)
+          totalGradeCount += i;
+
+        // Set the test scored averages
+        testScoreAvg = sumGrade / (double) totalGradeCount; 
+        System.out.println("Average test score: " + testScoreAvg);
+
+        for (int x = 0; x < 5; x++)
+          System.out.printf("%c:%5d%n", 
+              letterGrade[x], letterGradeCount[x]);
+
+        in.nextLine();
+}
 
   /**
    * Use "while", "do-while", & "for" loops
-   * print 
-   *  1 -> 10
-   *  10 -> 1
-   *  2 -> 20. Evens only
+   * print
+   * 1 -> 10
+   * 10 -> 1
+   * 2 -> 20. Evens only
    */
   static void problem3() {
     // For loop
     System.out.println("For loop from 1 -> 10");
-    for(int x = 0;x <= 10; x++)
+
+    for (int x = 1; x <= 10; x++)
       System.out.println(x);
 
-    System.out.println("");
-
-    System.out.println("For loop from 10 -> 1");
-    for(int x = 10;x > 0; x--)
+    System.out.println("\nFor loop from 10 -> 1");
+    for (int x = 10; x > 0; x--)
       System.out.println(x);
 
-    System.out.println("");
+    System.out.println("\nFor loop from 2 -> 20. Even numbers only");
 
-    System.out.println("For loop from 2 -> 20. Even number only");
-    for(int x = 2;x <= 20; x++)
-      if(x % 2 == 0)
+    for (int x = 2; x <= 20; x++)
+      if (x % 2 == 0)
         System.out.println(x);
-
-    System.out.println("");
 
     // do-while loop
-    int x = 0;
+    int x = 1;
+    System.out.println("\nDo-while from 1 -> 10");
     do {
       System.out.println(x);
-    } while(x++ < 10);
+      x++;
+    } while (x <= 10);
 
+    System.out.println("\nDo-while from 10 -> 1");
+    x = 10;
     do {
-      x = 10;
       System.out.println(x);
-    } while(x-- > 0);
+      x--;
+    } while (x > 0);
 
+    System.out.println("\nDo-while from 2 -> 20. Even numbers only");
+    x = 2;
     do {
-      x = 2;
-      if(x % 2 == 0)
+      if (x % 2 == 0)
         System.out.println(x);
-    } while(x++ < 20);
+      x++;
+    } while (x <= 20);
 
-    // // While loop
-    // while(x < 10) 
-    //   System.out.println(x++);
+    x = 0;
+    // While loop
+    System.out.println("\nWhile loop from 1 -> 10");
+    while(x < 10)
+      System.out.println(++x);
 
-    // while(x > 10) 
-    //   System.out.println(x--);
+    x = 10;
+    System.out.println("\nWhile loop from 10 -> 1");
+    while(x > 0)
+      System.out.println(x--);
 
-    // while(x < 20) 
-    //   if (x % 2 == 0)
-    //     System.out.println(x++);
-    
+    x = 2;
+    System.out.println("\nWhile loop from 2 -> 20. Even numbers only");
+    while(x <= 20) {
+      if (x % 2 == 0)
+        System.out.println(x);
+      x++;
+    }
+    System.out.println("");
+
   }
 
   /**
    * This program prompts the user for 5 destinations
-   * Store destination in an array
-   * Print 3 unique random destinations
+   * Print 3 unique random destinations out of the 5 entered
    */
   static void problem4() {
+    final int MAX_DESTINATION = 5;
+    // Use Random class to return a random number
     Random rand = new Random();
-    String [] destination = new String[5];
+    String[] destination = new String[MAX_DESTINATION];
     System.out.println("Enter 5 destinations");
-    for(int x = 0; x < 5; x++) {
-      destination[x] = in.next();   
+
+    // visited [] holds the indecis that that have been visited
+    boolean [] visited = new boolean [MAX_DESTINATION];
+    // Initialize as all false
+    Arrays.fill(visited, false);
+
+    // Input destinations
+    for (int x = 0; x < 5; x++) 
+      destination[x] = in.nextLine();
+
+    // Output
+    System.out.println("Your 3 random destinations");
+    int randIndex = rand.nextInt(destination.length);
+    int index = 0;
+    while(index < 3) {
+      if(!visited[randIndex]) {
+        System.out.println(destination[randIndex]);
+        // Set the index as visited
+        visited[randIndex] = true;
+        index++;
+      }
+      else
+        randIndex = rand.nextInt(destination.length);
     }
   }
 
-  /** 
-   * Generates a string menu based on the number of programs to choose from. Prepends the index (offset by 1) to the program name
+  /**
+   * Generates a string menu based on the number of programs to choose from.
+   * Prepends the index (offset by +1) by the program name
+   * 
    * @param String Array of program names
    * @return String Program names to run
-   * */ 
+   */
   public static String getMenu(String[] progNames) {
     String menu = "";
     for (int x = 0; x < progNames.length; x++)
       menu += Integer.toString(x + 1) + ". " + progNames[x] + '\n';
 
-    return "Choose a program to run\n" +
-           menu +
-           "0. Exit";
+    return 
+      "Choose a program to run\n" +
+      menu +
+      "0. Exit";
   }
-
-  /**
-   * Returns a character based on the grade value   
-   * @param grade Numerical grade
-   * @return char Capitalized letter grade
-   */
-  static char returnLetterGrade(int grade) {
-
-    char letterGrade = 'F';
-    if(grade < 60 && grade > 70)
-      letterGrade = 'D';
-    else if (grade < 70 && grade > 80)
-      letterGrade = 'C';
-    else if (grade < 90 && grade > 80)
-      letterGrade = 'B';
-    else if (grade > 90)
-      letterGrade = 'A';
-
-    return letterGrade;
-  }
-
 }
