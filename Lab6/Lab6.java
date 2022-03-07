664e3bcdb36c76eec913d137ac9429b64fbc1cfd
curlyLasagna <luis.gcodes@gmail.com>
2022-03-07 13:02:07 -0500

    Board now prints character instead of ASCII code

--- Lab6/Lab6.java
+++ Lab6/Lab6.java
@@ -42,6 +42,7 @@ public class Lab6 {
           default:
             System.err.println("Option does not exist. Try again");
             badInputCount++;
+            in.nextLine();
             break;
         }
       } else {
@@ -56,30 +57,37 @@ public class Lab6 {
   }
 
   static void problem1() {
-    // Probably a little over engineered
+    // Probably a little over engineered. Didn't see the tip until I finished this up
     in.nextLine();
     boolean run = true;
+    // If user only enters a single number
+    boolean singleDigit = false;
     double result = 0;
     System.out.println("This calculator takes simple calculations from a user");
+    final char [] opArr = {'+', '-', '/', '*'};
+    String [] splitExpression;
+
+    /* Regex patterns
+     * pattern = expression pattern
+     * delimOp = Sets arithmetic operations as a delimiter
+     */
+    final String 
+      pattern = "\\d+\\s*[+-/\\*]\\s*\\d+",
+      delimOp = "[+-/\\*]+";
 
     do {
       double lhs, rhs;
       lhs = rhs = 0;
       char op = ' ';
-      char [] opArr = {'+', '-', '/', '*'};
-
-      /* Regex patterns
-       * pattern = expression pattern
-       * delimOp = Sets arithmetic operations as a delimiter
-       */
-      String 
-        pattern = "\\d+\\s*[+-/\\*]\\s*\\d+",
-        delimOp = "[+-/\\*]+";
-
       String expression = in.nextLine();
   
       // Stores the two values to be evaluated
-      String [] splitExpression = expression.split(delimOp);
+      splitExpression = expression.split(delimOp);
+
+      if(splitExpression.length == 1) {
+        singleDigit = true;
+        break;
+      }
 
       // Check if pattern is correct (lhs op rhs)
       if(!expression.matches(pattern)) {
@@ -92,7 +100,7 @@ public class Lab6 {
 
       // Sets the operation of the expression
       for(int x = 0; x < opArr.length; x++)
-        if(expression.indexOf(opArr[x]) > 1)
+        if(expression.indexOf(opArr[x]) > 0)
           op = expression.charAt(expression.indexOf(opArr[x]));
 
       switch (op) {
@@ -123,20 +131,26 @@ public class Lab6 {
 
     } while (run);
 
-    // Only prints decimals if result contains decimals
-    if(result % 1 == 0)
-      System.out.printf("Result: %.0f%n%n", result);
-    else
-      System.out.printf("Result: %.2f%n%n", result);
-
+    if(singleDigit) {
+      System.out.println("Result: " + splitExpression[0]);
+    }
+    else {
+      // Only prints decimals if result contains decimals
+      if(result % 1 == 0)
+        System.out.printf("Result: %.0f%n%n", result);
+      else
+        System.out.printf("Result: %.2f%n%n", result);
+    }
   }
 
   static void problem2() {
+    System.out.println("");
     for (int i = 1; i < 6; i++) {
       for (int k = i; k <= i + 4; k++)
         System.out.print(k + " ");
       System.out.println("");
     }
+    System.out.println("");
   }
 
   static void problem3() {
@@ -146,14 +160,19 @@ public class Lab6 {
     
     boolean quit = false;
     char [][] board = new char [ROW_SIZE][COL_SIZE];
-    char point = 254;
+    final char 
+      point = 256,
+      unPoint = '-';
     int xPos, yPos, movDist = 0;
-    xPos = yPos = 5;
+    xPos = yPos = 4;
 
     // Fill board with dashes
     for (char [] b : board)
-      Arrays.fill(b, '-');
+      Arrays.fill(b, unPoint);
       
+    // Initial position
+    board[xPos][yPos] = point;
+
      printBoard(board);
      while (!quit) {
       char input = ' ';
@@ -161,31 +180,37 @@ public class Lab6 {
       System.out.println("Enter the direction you would like to move? (N S E W)\nType 'quit' to end");
       input = in.next().charAt(0);
       Character.toUpperCase(input);
+
       System.out.printf("Enter the number of spaces you would like to move %c%n", input);
       movDist = in.nextInt();
       
       switch (input){
         case 'N': 
-          board[xPos][yPos] = '-';
-          yPos++;
+          board[xPos][yPos] = unPoint;
+          yPos+=movDist;
+          yPos = wrapCols(yPos, 9, 9);
           board[xPos][yPos] = point;
           break;
         case 'S':
-          board[xPos][yPos] = '-';
-          yPos--;
+          board[xPos][yPos] = unPoint;
+          yPos-=movDist;
+          yPos = wrapCols(yPos, 9, 9);
           board[xPos][yPos] = point;
           break;
         case 'E':
-          board[xPos][yPos] = '-';
-          xPos++;
+          board[xPos][yPos] = unPoint;
+          xPos+=movDist;
+          xPos = wrapRows(xPos, 9, 9);
           board[xPos][yPos] = point;
           break;
-          case 'W':
-          board[xPos][yPos] = '-';
-          xPos--;
+        case 'W':
+          board[xPos][yPos] = unPoint;
+          xPos-=movDist;
+          xPos = wrapRows(xPos, 9, 9);
           board[xPos][yPos] = point;
           break;
         }
+         printBoard(board);
      }
   }
 
@@ -206,34 +231,36 @@ public class Lab6 {
         "0. Exit";
   }
 
-  // public static int wrapRows(int pos) {
-  // //Wraps from Top Edge to Bottom Edge
-  // if(pos < 0)
-  // return MAX_ROWS-1;
-  // //Wraps from Bottom Edge to Top Edge
-  // else if(pos == MAX_ROWS || pos > MAX_ROWS) {
-  // return 0;
-  // }
-  // else
-  // return pos;
-  // }
+  public static int wrapRows(int pos, int MAX_ROWS, int MAX_COLS) {
+    // Wraps from Top Edge to Bottom Edge
+    if(pos < 0)
+      return MAX_ROWS-1;
 
-  // public static int wrapCols(int pos) {
-  // if(pos < 0)
-  // return MAX_COLS-1;
-  // //Wraps from Left Edge to Right Edge
-  // else if(pos == MAX_COLS || pos > MAX_COLS)
-  // return 0;
-  // //Wraps from Right Edge to Left Edge
-  // else
-  // return pos;
-  // }
-  //
+    // Wraps from Bottom Edge to Top Edge
+    else if(pos == MAX_ROWS || pos > MAX_ROWS) 
+      return 0;
+    
+    else
+      return pos;
+  }
+
+   public static int wrapCols(int pos, int MAX_ROWS, int MAX_COLS) {
+     if(pos < 0)
+       return MAX_COLS-1;
+
+     // Wraps from Left Edge to Right Edge
+     else if(pos == MAX_COLS || pos > MAX_COLS)
+       return 0;
+
+     // Wraps from Right Edge to Left Edge
+     else
+       return pos;
+   }
   
   public static void printBoard(char[][] board) {
     for (int x = 0; x < 9; x++) {
       for (int y = 0; y < 9; y++) {
-        System.out.print(board[x][y] + ' ');
+        System.out.printf("%c ", board[x][y]);
       }
       System.out.println("");
     }
