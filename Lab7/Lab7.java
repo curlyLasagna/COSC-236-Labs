4b39a6228c2b3bbe937cb04303945c2bee8ed292
curlyLasagna <luis.gcodes@gmail.com>
2022-03-16 19:35:47 -0400

    Finished problem 1. Menu system still buggy

--- Lab7/Lab7.java
+++ Lab7/Lab7.java
@@ -2,58 +2,135 @@ import java.util.*;
 import java.io.*;
 
 public class Lab7 {
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
+  static Scanner in = new Scanner(System.in);
+  public static void main(String[] pneumonoultramicroscopicsilicovolcanoconiosis) throws FileNotFoundException , IOException{
+
+    while(menu());
+  
+  }
+
+  /* Reads from "input.txt" to calculate the sum of the numbers in the file
+   * Identifies the sum, maximum and the average of the values read from file
+   *
+   * */
+  static void read_from_file() throws FileNotFoundException, IOException {
+    Scanner rf = new Scanner(new File("input.txt"));
+
+    int countNum = 0;
+
+    double 
+      sum = 0,
+      max = 0,
+      currentNum = 0;
+
+    while (rf.hasNext()) {
+      max = currentNum;
+      currentNum = rf.nextDouble();
+
+      if(max < currentNum)
+        max = currentNum;
+
+      sum += currentNum;
+      countNum++;
+    }
+
+    rf.close();
+    write_to_file(sum, max, sum / countNum);
+
+  }
+
+  /* Output sum, max, and average to output.txt
+   * Not a smart way of using variable args as I only need 3 args
+   * but didn't feel like typing out 3 separate arg names on the function header
+   * @param 3 double values 
+   * */
+  static void write_to_file(double ...d) throws FileNotFoundException, IOException{
+    PrintStream out = new PrintStream(new File("output.txt"));
+    out.printf("Sum: %.2f%nMax: %.2f%nAvg: %.2f%n", d[0], d[1], d[2]);
+    menu();
+  }
+
+  static void write_StudentRec() throws FileNotFoundException, IOException {
+    FileOutputStream fs = new FileOutputStream("students.dat");
+    BufferedOutputStream bs = new BufferedOutputStream(fs);
+    DataOutputStream ds = new DataOutputStream(bs);
+    int studentCount = 0;
+
+    String 
+      firstName = "( ͡° ͜ʖ ͡°)",
+      lastName = "[̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅]",
+      year = "¯\\_( ͡° ͜ʖ ͡°)_/¯";
+
+    System.out.println("How many heads");
+    studentCount = in.nextInt();
+
+    for (int x = 1; x <= studentCount; x++) {
+      System.out.println("Enter student " + x + " first name: ");
+      firstName = in.next();
+      System.out.println("Enter student " + x + " last name: ");
+      lastName = in.next();
+      System.out.println("What year did " + firstName + " take this course?");
+      year = in.next();
+
+      ds.writeBytes(firstName + '\n' + lastName + '\n' + year);
+    }
+
+    fs.close();
+    bs.close();
+    ds.close();
+
+    menu();
+  }
+
+  static void read_StudentRec() throws FileNotFoundException, IOException{
+
+    menu();
+  }
+
+  /**
+   * @return boolean whether to continue running the program
+   * @throws FileNotFoundException IOException
+   */
+  static boolean menu() throws FileNotFoundException, IOException {
 
     final String [] progNames = {
-      "Read and Sum",
+      "Get file statistics",
       "Print student information",
       "Read student data"
     };
 
-    while(runProg(in, progNames));
-
-  }
-
-  /* Reads from "input.txt" to calculate the sum of the numbers in the file
-   * Identify the maximum value and the average 
-   *
-   * @return double array containing the sum, max and average 
-   * */
-  static void read_from_file() {
-    Scanner rf = new Scanner(new File("input.txt"));
-  }
-
-  /* Output sum, max, and average
-   *
-   * @param arr double array from read_from_file
-   * */
-  static void write_to_file(double [] arr) {
-    PrintStream out = new PrintStream(new File("output.txt"));
-  }
-
-  static void write_StudentRec() {
-
-  }
-
-  static void read_StudentRec() {
-
-  }
-
-  static boolean runProg(Scanner in, String [] progNames) {
-    boolean run = true;
-
     System.out.println(miscFunc.getMenu(progNames));
+    System.out.print("> ");
 
     if(in.hasNextInt()) {
       int choice = 0;
-      System.out.print("> ");
+      choice = in.nextInt();
       switch(choice) {
-        case 1 -> read_from_file()
-        case 2 -> write_StudentRec()
-        case 3 -> read_StudentRec()
+        case 1: 
+          read_from_file();
+          break;
+        case 2:  
+          write_StudentRec();
+          break;
+        case 3:  
+          read_StudentRec();
+          break;
+        case 0:  
+          System.out.println("Thanks for using our program");
+          break;
+        default:
+          System.err.println("Invalid option. Try again"); 
+          in.nextLine();
       }
     }
-    return run;
+
+    else {
+      System.err.println("Invalid option. Try again");
+      in.nextLine();
+    }
+
+    in.nextLine();
+
+    return false;
   }
 }
