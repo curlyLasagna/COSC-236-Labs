c97db8ff03affd50fd13d1e3648d8657d4d844ca
curlyLasagna <luis.gcodes@gmail.com>
2022-04-02 03:20:53 -0400

    learning gradle

--- lab8/Lab8.java
+++ lab8/Lab8.java
@@ -1,114 +1,121 @@
-    11	import java.util.Scanner;
-    12	
-    13	public class IR4 {
-    14	   //Putting the Scanner object here makes it global so it does not have to be passed to modules.
-    15	   static Scanner keyboard = new Scanner(System.in);
-    16	   
-    17	   public static void main(String[] args) {
+import java.util.Scanner;
 
-    47	   }
-    48	   
-    49	   public static void displayGoodbye(){
-    50	       System.out.println("Goodbye!");
-    51	   }
-    52	   
-    57	   /** Gets an integer greater than and less than the supplied parameters. 
-    58	     * Rejects null entries, any number of spaces, and non-numbers.
-    59	     * @param msg is the text that will be displayed the user to ask them to enter a value.
-    60	     * @param low is the lowest acceptable input value.
-    61	     * @param high is the highest acceptable input value.
-    62	     * @return Returns an int from the keyboard. 
-    63	     */
-    64	   public static int getIntegerBetweenLowAndHigh (Scanner keyboard, String msg, int low, int high) {
-    65	      int number = getInteger(msg);
-    66	      while (number < low || number > high) {
-    67	         System.err.println(errorMsg);
-    68	         number = getInteger(msg);
-    69	      }
-    70	      return number;
-    71	   }
+public class Lab8 {
+   
+   public static void main(String[] args) {
+      Scanner keyboard = new Scanner(System.in);
+      getString(keyboard, "Please give me the name of any item");
+      getString(keyboard, "Enter a color name");
+      getString(keyboard, "Please enter the name of a different color");
+      getInt(keyboard, "What is the maximum number of rare giant toenail's can someone have");
+      getInt(keyboard, "What is the minimum number of rare giant toenail's can someone have");
+      getDouble(keyboard, "What is the maximum price someone might pay for a giant toenail");
 
-    64	   public static double getDoubleBetweenLowAndHigh (Scanner keyboard, String msg, double low, double high) {
-    65	      double number = getDouble(msg);
-    66	      while (number < low || number > high) {
-    67	         System.err.println(errorMsg);
-    68	         number = getDouble(msg);
-    69	      }
-    70	      return number;
-    71	   }
-    72	   
-   106	   /** Gets a String from the keyboard. Rejects null entry or any number of spaces.
-   107	     * @param msg is the text that will be displayed the user to ask them to enter a value.
-   108	     * @return Returns a String from the keyboard. 
-   109	     */ 
-   110	   public static String getString(Scanner keyboard, String msg) {
-   111	      String answer = "";
-   112	      System.out.println(msg);
-   113	      try {
-   114	         answer = keyboard.nextLine(); 
-   115	      }
-   116	      catch (Exception e) {
-   117	         System.err.println("Error reading input from user. Ending program.");
-   118	         System.exit(-1);
-   119	      } 
-   120	      
-   121	      while (answer.replace(" ", "").equals("")) {
-   122	         System.err.println("Error: Missing input.");
-   123	         try {
-   124	            System.out.println(msg);
-   125	            answer = keyboard.nextLine(); 
-   126	         }
-   127	         catch (Exception e) {
-   128	            System.err.println("Error reading input from user. Ending program.");
-   129	            System.exit(-1);
-   130	         } 
-   131	      }
-   132	      return answer;            
-   133	   }
-   134	   
-   135	   //------------------------------------------------------------------------------------------------------------------
-   136	   /** Gets an Integer from the keyboard. Rejects null, spaces and non-integers.
-   137	     * @param msg is the text that will be displayed the user to ask them to enter a number.
-   138	     * @return Returns an int from the keyboard. 
-   139	     */  
-   140	   public static int getInteger(Scanner keyboard, String msg) {
-   141	      System.out.println(msg);
-   142	      while (!keyboard.hasNextInt()) {
-   143	         keyboard.nextLine();
-   144	         System.err.println("Invalid integer. Try again.");
-   145	      }
-   146	      int number = keyboard.nextInt();
-   147	      keyboard.nextLine(); //flushes the buffer
-   148	      return number;
-   149	   }
-   150	   
-   151	   //------------------------------------------------------------------------------------------------------------------
-   152	   /** Gets a Double from the keyboard. Rejects null, spaces and non-numbers.
-   153	     * @param msg is the text that will be displayed the user to ask them to enter a number.
-   154	     * @return Returns a double from the keyboard. 
-   155	     */ 
-   156	   public static double getDouble(Scanner keyboard, String msg) {
-   157	      System.out.println(msg);
-   158	      while (!keyboard.hasNextDouble()) {
-   159	         keyboard.nextLine();
-   160	         System.err.println("Invalid number. Try again.");
-   161	      }
-   162	      double number = keyboard.nextDouble();
-   163	      keyboard.nextLine(); //flushes the buffer
-   164	      return number;
-   165	   }
-   166	   
-   202	   //------------------------------------------------------------------------------------------------------------------
-   203	   /** Closes the scanner.
-   204	     */ 
-   205	   public static void closeScanner() {
-   206	      try { 
-   207	         if(keyboard != null) {
-   208	            keyboard.close(); 
-   209	         }
-   210	      } 
-   211	      catch (Exception e) { // (Exception) catches all errors java might throw here
-   212	         System.err.println("Error closing reader.");
-   213	      }
-   214	   }
-   225	}
+      keyboard.close();
+   }
+
+   /** Method 1 
+     * Gets a String from the keyboard. 
+     * @param msg is the text that will be displayed the user to ask them to enter a value.
+     * @return Returns a String from the keyboard. 
+     */ 
+   public static String getString(Scanner keyboard, String msg) {
+      String answer = "";
+      System.out.println(msg);
+      try {
+         answer = keyboard.nextLine(); 
+      }
+      catch (Exception e) {
+         System.err.println("Error reading input from user. Ending program.");
+         System.exit(-1);
+      } 
+      
+      while (answer.replace(" ", "").equals("")) {
+         System.err.println("Error: Missing input.");
+         try {
+            System.out.println(msg);
+            answer = keyboard.nextLine(); 
+         }
+         catch (Exception e) {
+            System.err.println("Error reading input from user. Ending program.");
+            System.exit(-1);
+         } 
+      }
+      return answer;            
+   }
+
+   /** Method 2
+     * Gets an Int from the keyboard. 
+     * @param msg is the text that will be displayed the user to ask them to enter a number.
+     * @return Returns an int from the keyboard. 
+     */  
+   public static int getInt(Scanner keyboard, String msg) {
+      System.out.println(msg);
+
+      while (!keyboard.hasNextInt()) {
+         keyboard.nextLine();
+         System.err.println("Invalid integer. Try again.");
+      }
+
+      int number = keyboard.nextInt();
+      keyboard.nextLine();
+      return number;
+   }
+
+   /** Method 3 
+     * Gets an integer greater than and less than the supplied parameters. 
+     * @param msg is the text that will be displayed the user to ask them to enter a value.
+     * @param low is the lowest acceptable input value.
+     * @param high is the highest acceptable input value.
+     * @return Returns an int from the keyboard. 
+     */
+   public static int getIntBetweenLowAndHigh (
+      Scanner keyboard, 
+      String msg, 
+      String errorMsg, 
+      int low, int high) {
+         int number = getInt(keyboard, msg);
+         while (number < low || number > high) {
+            System.err.println(errorMsg);
+            number = getInt(keyboard, msg);
+      }
+         return number;
+   }
+
+   /** Method 4
+     * Gets a Double from the keyboard. 
+     * @param msg is the text that will be displayed the user to ask them to enter a number.
+     * @return Returns a double from the keyboard. 
+     */ 
+   public static double getDouble(Scanner keyboard, String msg) {
+      System.out.println(msg);
+      while (!keyboard.hasNextDouble()) {
+         keyboard.nextLine();
+         System.err.println("Invalid number. Try again.");
+      }
+      double number = keyboard.nextDouble();
+      keyboard.nextLine();
+      return number;
+   }
+
+   /** Method 5 
+     * Gets a double greater than and less than the supplied parameters. 
+     * @param msg is the text that will be displayed the user to ask them to enter a value.
+     * @param low is the lowest acceptable input value.
+     * @param high is the highest acceptable input value.
+     * @return Returns an int from the keyboard. 
+     */
+   public static double getDoubleBetweenLowAndHigh (
+      Scanner keyboard, 
+      String msg, 
+      String errorMsg, 
+      double low, double high) {
+         double number = getDouble(keyboard, msg);
+         while (number < low || number > high) {
+            System.err.println(errorMsg);
+            number = getDouble(keyboard, msg);
+      }
+         return number;
+   }
+   
+}
